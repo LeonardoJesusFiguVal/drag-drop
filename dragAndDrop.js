c3ab6a723a06edf1de3f08dca6a2b049a82740a4
leonardoJesusFiguVal <leonardojfigueroaval@gmail.com>
2023-06-02 17:51:40 +0100

    Readme added

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -7,15 +7,12 @@
  * @author Lucas Sanchez Cabrera
  * @author Jonathan Rodriguez Cruz
  */
-
-//Seleccionamos los elementos fill y empty del documento
 const fill= document.querySelector(".fill");
 const empties= document.querySelectorAll(".empty");
 
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-//Asignar a todos los elementos empty los event listener
 for (const empty of empties){
     empty.addEventListener("dragover", dragOver);
     empty.addEventListener("dragenter", dragEnter);
@@ -23,35 +20,24 @@ for (const empty of empties){
     empty.addEventListener("drop", dragDrop);
 }
 
-//Cambia la clase fill por invisible
 function dragStart() {
     this.className+= " hold";
     setTimeout(() => this.className= "invisible", 0);
     
 }
-
-//Cambia el div fill a dentro de otro div
 function dragEnd(){
     this.className= "fill";
 }
-
-
 function dragOver(e){
     e.preventDefault();
 }
-
-
 function dragEnter(e){
     e.preventDefault();
     this.className+= " hovered";
 }
-
-//Cambia el div al que le quitamos la imagen a la clase empty
 function dragLeave(){
     this.className= "empty";
 }
-
-
 function dragDrop(){
     this.className= "empty";
     this.append(fill);
